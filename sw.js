d8e9540ec47915a04dd9c98321988d37e8c9f373
Raticos <51394970+Raticos@users.noreply.github.com>
2020-11-03 13:18:27 +1030

    d

---
+++ sw.js
@@ -0,0 +1,36 @@
+self.addEventListener('insstall', event => {
+    console.log('Install event!');
+})
+
+self.addEventListener('activate', event => {
+    console.log('Activate event!');
+})
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch intercepted for:', event.request.url);
+})
+
+var cacheName = 'cache-v1';
+var resourcesToPrecache = [
+    '/',
+    'index.html',
+    'images/front.jpg'
+];
+
+self.addEventListener('install', event => {
+    console.log('Service worker install event!');
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(resourcesToPrecache);
+            })
+    );
+})
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request)
+        .then(cachedResponse => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+})
